7450461811c724e6350a1a954d3c7fb128bfcb3a
Justin Chambers <justinmchambers@gmail.com>
2018-02-22 18:54:48 -0800

    run_analysis.R v2 complete

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,18 @@
 library(data.table)
 library(dplyr)
 
-tr_subjects <- data.frame(fread("./data/train/subject_train.txt", header = FALSE))
-ts_subjects <- data.frame(fread("./data/test/subject_test.txt", header = FALSE))
-tr_features <- data.frame(fread("./data/train/X_train.txt", sep = " ", header = FALSE))
-ts_features <- data.frame(fread("./data/test/X_test.txt", sep = " ", header = FALSE))
-tr_labels <- data.frame(fread("./data/train/y_train.txt", header = FALSE))
-ts_labels <- data.frame(fread("./data/test/y_test.txt", header = FALSE))
-var_names <- fread("./data/features.txt", sep = " ", header = FALSE)
-obs_desc <- fread("./data/activity_labels.txt", sep = " ", header = FALSE)
+initDataFrame <- function(file_path) {
+        df <- fread(file_path, sep = " ", header = FALSE, data.table = FALSE, stringsAsFactors = FALSE)
+}
+
+tr_subjects <- initDataFrame("./data/train/subject_train.txt")
+ts_subjects <- initDataFrame("./data/test/subject_test.txt")
+tr_features <- initDataFrame("./data/train/X_train.txt")
+ts_features <- initDataFrame("./data/test/X_test.txt")
+tr_labels <- initDataFrame("./data/train/y_train.txt")
+ts_labels <- initDataFrame("./data/test/y_test.txt")
+var_names <- initDataFrame("./data/features.txt")
+obs_desc <- initDataFrame("./data/activity_labels.txt")
 colnames(tr_features) <- var_names$V2
 colnames(ts_features) <- var_names$V2
 tr_labels <- sapply(tr_labels$V1, function(el) { tolower(obs_desc[ el, 2]) })
@@ -17,11 +21,14 @@ tr_features <- tr_features[ , grep("[Mm]ean\\(|[Ss]td\\(", colnames(tr_features)
         mutate(activityType := tr_labels) %>%
         mutate(subjectNumber := as.integer(tr_subjects[, 1]))
 ts_features <- ts_features[ , grep("[Mm]ean\\(|[Ss]td\\(", colnames(ts_features))] %>% 
-        mutate(activityType := ts_labels)%>%
+        mutate(activityType := ts_labels) %>%
         mutate(subjectNumber := as.integer(ts_subjects[, 1]))
+format(tr_features[ , 1], scientific = TRUE, format = "e", digits = 1)
 m <- merge(tr_features, ts_features, all = TRUE)
 rm(ts_labels, ts_features, ts_subjects, tr_labels, tr_features, tr_subjects, var_names, obs_desc)
-out <- m %>% group_by(subjectNumber, activityType) %>% summarise_all(funs(average = mean))
+out <- m %>% group_by(subjectNumber, activityType) %>% 
+        summarise_all(funs(average = mean))
+out <- format.data.frame(out, scientific = TRUE)
 write.table(out, "./selected_feature_averages_by_subject_and_activity.txt", row.name = FALSE)
 rm(m, out)
 print("Analysis complete. Check current directory for output file.")
